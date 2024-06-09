5d730220751e93e52e1d4e80af1a8671ba1af8cd
Atia9f <atiafairujakhi2003@gmail.com>
2024-06-09 11:56:37 +0600

    get donation req

--- index.js
+++ index.js
@@ -222,6 +222,7 @@ async function run() {
         res.send(error);
       }
     });
+
     app.get("/donation-request/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
